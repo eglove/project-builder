c7c95a3c106c4c652459e6e7942d432bae84762d
Ethan Glover <hello@ethang.email>
2023-12-03 14:40:02 -0600

    "Uncomment code and add dependencies for better build process
    The commented code responsible for running the `tsc` command and handling tsConfigOverrides have been uncommented and necessary dependencies added in the `src/build-project.js` script, to facilitate the project build process. This update ensures a smooth transition between different tsConfig setup during the build and reduces potential errors in the output files."

--- src/build-project.ts
+++ src/build-project.ts
@@ -2,6 +2,9 @@ import * as fs from 'node:fs';
 
 import { rimraf } from 'rimraf';
 import tsup from 'tsup';
+import {runCommand} from "./run-command.js";
+import {deepMerge} from "@ethang/util/object.js";
+import {gitUpdate} from "./git-update.js";
 
 export async function buildProject(
   publishDirectory: string,
@@ -10,24 +13,23 @@ export async function buildProject(
 ) {
   await rimraf(publishDirectory);
 
-  // if (tsConfigOverrides === undefined) {
-  //   runCommand(`tsc --project tsconfig.json`);
-  // } else {
-  //   const tsConfigString = fs.readFileSync('tsconfig.json', {
-  //     encoding: 'utf8',
-  //   });
-  //   const originalTsConfig = JSON.parse(tsConfigString);
-  //
-  //   const merged = deepMerge(originalTsConfig, tsConfigOverrides);
-  //   fs.writeFileSync('tsconfig.build.json', JSON.stringify(merged, null, 2));
-  //
-  //   await gitUpdate('Generate tsConfig');
-  //
-  //   runCommand('tsc --project tsconfig.build.json');
-  // }
+  if (tsConfigOverrides === undefined) {
+    runCommand(`tsc --project tsconfig.json`);
+  } else {
+    const tsConfigString = fs.readFileSync('tsconfig.json', {
+      encoding: 'utf8',
+    });
+    const originalTsConfig = JSON.parse(tsConfigString);
+
+    const merged = deepMerge(originalTsConfig, tsConfigOverrides);
+    fs.writeFileSync('tsconfig.build.json', JSON.stringify(merged, null, 2));
+
+    await gitUpdate('Generate tsConfig');
+
+    runCommand('tsc --project tsconfig.build.json');
+  }
 
   await tsup.build({
-    dts: { entry: tsupOptions.entry },
     format: ['cjs', 'esm'],
     minify: true,
     sourcemap: true,
