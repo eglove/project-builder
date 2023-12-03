e851700e7c5ad80f0696535fab7d622b904cb3e5
Ethan Glover <hello@ethang.email>
2023-12-03 14:29:41 -0600

    "Refactor build scripts and add ESLint
    Removed several unnecessary imports from 'build.mjs' and 'build-project.ts', as well as commented out unused code block in the latter, refactoring the project's build scripts. Added 'eslint . --fix' to package.json, providing more defense against potential syntax errors and improving overall code quality."

--- build.mjs
+++ build.mjs
@@ -1,63 +1,7 @@
-import { rimraf } from 'rimraf'
-import { execSync } from 'child_process'
-import tsup from 'tsup'
-import fs from 'node:fs'
-import inquirer from 'inquirer'
-import { simpleGit } from 'simple-git'
-import chalk from 'chalk'
+import { projectBuilder } from './dist/project-builder.js'
 
-console.info(chalk.bgBlue.white('Running for project-builder'))
-
-const git = simpleGit()
-const status = await git.status()
-
-if (!status.isClean()) {
-  console.error('Commit your changes!')
-  process.exit()
-}
-
-await rimraf('dist')
-
-execSync('tsc --project tsconfig.json')
-
-await tsup.build({
-  bundle: true,
-  minify: true,
-  outDir: 'dist',
-  format: ['cjs', 'esm'],
-  entry: ['src/*'],
+await projectBuilder('project-builder', 'master', {
+  preVersionBumpScripts: ['UPDATE', 'PRUNE'],
+  postVersionBumpScripts: ['DEDUPE', 'LINT'],
+  publishDirectory: 'dist',
 })
-
-const { semver } = await inquirer.prompt([
-  {
-    choices: ['patch', 'minor', 'major', 'no-publish'],
-    message: 'SemVer',
-    name: 'semver',
-    type: 'list',
-  },
-])
-
-if (semver !== 'no-publish') {
-  execSync(`npm version ${semver}`)
-}
-
-const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'))
-packageJson.peerDependencies = packageJson.dependencies
-fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2) + '\n', 'utf8')
-
-const peerDepStatus = await git.status()
-if (!peerDepStatus.isClean()) {
-  await git.add('.')
-  await git.add('Peer Dependency Update')
-}
-
-fs.copyFileSync(
-  'package.json',
-  'dist/package.json',
-)
-
-if (semver !== 'no-publish') {
-  execSync('cd dist && npm publish --access public && cd ..')
-}
-
-await git.push()
--- src/build-project.ts
+++ src/build-project.ts
@@ -1,11 +1,7 @@
 import * as fs from 'node:fs';
 
-import { deepMerge } from '@ethang/util/object.js';
 import { rimraf } from 'rimraf';
 import tsup from 'tsup';
-
-import { gitUpdate } from './git-update.ts';
-import { runCommand } from './run-command.ts';
 
 export async function buildProject(
   publishDirectory: string,
@@ -14,23 +10,29 @@ export async function buildProject(
 ) {
   await rimraf(publishDirectory);
 
-  if (tsConfigOverrides === undefined) {
-    runCommand(`tsc --project tsconfig.json`);
-  } else {
-    const tsConfigString = fs.readFileSync('tsconfig.json', {
-      encoding: 'utf8',
-    });
-    const originalTsConfig = JSON.parse(tsConfigString);
+  // if (tsConfigOverrides === undefined) {
+  //   runCommand(`tsc --project tsconfig.json`);
+  // } else {
+  //   const tsConfigString = fs.readFileSync('tsconfig.json', {
+  //     encoding: 'utf8',
+  //   });
+  //   const originalTsConfig = JSON.parse(tsConfigString);
+  //
+  //   const merged = deepMerge(originalTsConfig, tsConfigOverrides);
+  //   fs.writeFileSync('tsconfig.build.json', JSON.stringify(merged, null, 2));
+  //
+  //   await gitUpdate('Generate tsConfig');
+  //
+  //   runCommand('tsc --project tsconfig.build.json');
+  // }
 
-    const merged = deepMerge(originalTsConfig, tsConfigOverrides);
-    fs.writeFileSync('tsconfig.build.json', JSON.stringify(merged, null, 2));
-
-    await gitUpdate('Generate tsConfig');
-
-    runCommand('tsc --project tsconfig.build.json');
-  }
-
-  await tsup.build(tsupOptions);
+  await tsup.build({
+    dts: { entry: tsupOptions.entry },
+    format: ['cjs', 'esm'],
+    minify: true,
+    sourcemap: true,
+    ...tsupOptions,
+  });
 
   fs.copyFileSync('package.json', `${publishDirectory}/package.json`);
 }
